6be35ded7f112b6122047b763360f4e96d007b62
swamysurya <swamysurya1@gmail.com>
2024-10-19 23:06:43 +0530

    added bootsrap to the react

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // Import 'path' using ESM
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // root: path.resolve(__dirname, "src"), // Use ESM 'path' import
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"), // Alias for Bootstrap
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true, // Enable HMR (hot module replacement)
+    watch: {
+      usePolling: true, // Useful if you face issues with HMR not working due to file changes not being detected
+    },
+  },
+});
